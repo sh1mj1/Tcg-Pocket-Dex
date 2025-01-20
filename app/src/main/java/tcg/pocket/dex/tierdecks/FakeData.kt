5fc9bfdec7c392d8526e39afadecd57d3115d9ef
sh1mj1 <7wlgns@gmail.com>
2025-01-21 00:06:29 +0900

    feat(PokemonTypeChip): Add type chip component with customizable size

---
+++ app/src/main/java/tcg/pocket/dex/tierdecks/FakeData.kt
@@ -0,0 +1,21 @@
+package tcg.pocket.dex.tierdecks
+
+import tcg.pocket.dex.R
+
+val temporalPokemonPlaceholderDrawable: Int = R.drawable.tcg_pocket_unknown
+
+val fakeFightingTypeChipData =
+    PokemonTypeChipData(
+        imageUrl = "https://static.dotgg.gg/pokepocket/icons/fighting.png",
+        count = 1,
+    )
+val fakeFirePokemonTypeChipData =
+    PokemonTypeChipData(
+        imageUrl = "https://static.dotgg.gg/pokepocket/icons/fire.png",
+        count = 1,
+    )
+val fakeWaterPokemonTypeChipData =
+    PokemonTypeChipData(
+        imageUrl = "https://static.dotgg.gg/pokepocket/icons/water.png",
+        count = 1,
+    )
---
+++ app/src/main/java/tcg/pocket/dex/ui/theme/ChipSize.kt
@@ -0,0 +1,9 @@
+package tcg.pocket.dex.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object ChipSize {
+    val Small = 24.dp
+    val Medium = 32.dp
+    val Large = 48.dp
+}
