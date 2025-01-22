9d3dea040b8ce8334fa342281f2bf51c58ce5ea3
sh1mj1 <7wlgns@gmail.com>
2025-01-22 16:20:35 +0900

    feat(SearchActivity, SearchScreen): not implemented yet

---
+++ app/src/main/java/tcg/pocket/dex/search/SearchScreen.kt
@@ -0,0 +1,27 @@
+package tcg.pocket.dex.search
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import tcg.pocket.dex.ui.theme.TcgPocketDexTheme
+
+@Composable
+fun SearchScreen(modifier: Modifier = Modifier) {
+    // TODO: Implement search screen
+    Text(
+        text = "Search Screen",
+        modifier = modifier,
+        style = MaterialTheme.typography.displayLarge,
+    )
+}
+
+@Preview
+@Composable
+private fun SearchScreenPreview() {
+    TcgPocketDexTheme {
+        Surface { SearchScreen() }
+    }
+}
