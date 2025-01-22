9ed3ea5ce593497ddd9817214cec38cc995b237b
sh1mj1 <7wlgns@gmail.com>
2025-01-22 16:12:24 +0900

    feat(TireDecksScreen): add CenterAlignedTopAppBar

--- app/src/main/java/tcg/pocket/dex/tierdecks/TierDecksScreen.kt
+++ app/src/main/java/tcg/pocket/dex/tierdecks/TierDecksScreen.kt
@@ -1,7 +1,19 @@
 package tcg.pocket.dex.tierdecks
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import tcg.pocket.dex.ui.theme.TcgPocketDexTheme
 
@@ -11,15 +23,48 @@ val deckItemsState =
         addAll(fakeDecksInformation.map { DeckItemState(it) })
     }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TierDecksScreen() {
-    DeckList(
-        deckItemsState = deckItemsState,
-        onExpandDeck = { deckItemState, expanded ->
-            deckItemState.toggleExpanded()
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text("TCG Pocket Dex") },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        // TODO: navigate to search screen
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = null,
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = {
+                        // TODO: for the setting of the theme
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.Settings,
+                            contentDescription = null,
+                        )
+                    }
+                },
+            )
         },
-        onDeckItemClick = {},
-    )
+    ) { innerPadding ->
+        DeckList(
+            deckItemsState = deckItemsState,
+            onExpandDeck = { deckItemState, expanded ->
+                deckItemState.toggleExpanded()
+            },
+            onDeckItemClick = {},
+            modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .padding(innerPadding),
+        )
+    }
 }
 
 @Preview
