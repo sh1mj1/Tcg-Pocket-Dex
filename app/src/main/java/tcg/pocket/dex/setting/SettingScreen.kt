ec10005f37d346f0a4dac8d6a1662d1c4c3e5866
sh1mj1 <7wlgns@gmail.com>
2025-01-22 17:01:11 +0900

    feat(SettingActivity, SettingScreen): not implemented yet sh1mj1 40 minutes ago

---
+++ app/src/main/java/tcg/pocket/dex/setting/SettingScreen.kt
@@ -0,0 +1,25 @@
+package tcg.pocket.dex.setting
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import tcg.pocket.dex.ui.theme.TcgPocketDexTheme
+
+@Composable
+fun SettingScreen(modifier: Modifier = Modifier) {
+    Text(
+        text = "Setting",
+        style = MaterialTheme.typography.displayLarge,
+    )
+}
+
+@Preview
+@Composable
+private fun SettingScreenPreview() {
+    TcgPocketDexTheme {
+        Surface { SettingScreen() }
+    }
+}
