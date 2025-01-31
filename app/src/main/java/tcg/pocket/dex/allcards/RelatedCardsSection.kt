ff38e0db927b0392e4e86d9dfc1c27f79bda5d81
sh1mj1 <7wlgns@gmail.com>
2025-01-31 22:20:12 +0900

    refactor(RelatedCardsSection): reuse PocketDexSectionHeader

--- app/src/main/java/tcg/pocket/dex/allcards/RelatedCardsSection.kt
+++ app/src/main/java/tcg/pocket/dex/allcards/RelatedCardsSection.kt
@@ -5,15 +5,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import tcg.pocket.dex.component.PocketDexSectionHeader
 import tcg.pocket.dex.tierdecks.fakeCardsData
 import tcg.pocket.dex.ui.theme.TcgPocketDexTheme
 
@@ -24,23 +21,10 @@ fun RelatedCardsSection(
     modifier: Modifier = Modifier,
 ) {
     Column(modifier = modifier) {
-        Surface(
-            tonalElevation = 2.dp,
-            shadowElevation = 2.dp,
-            modifier =
-                Modifier
-                    .fillMaxWidth(),
-            shape = RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp),
-        ) {
-            Text(
-                text = "Related Cards",
-                style =
-                    MaterialTheme.typography.titleMedium.copy(
-                        fontWeight = FontWeight.Bold,
-                    ),
-                modifier = Modifier.padding(4.dp),
-            )
-        }
+        PocketDexSectionHeader(
+            text = "Related Cards",
+        )
+
         Surface(
             tonalElevation = 2.dp,
             shadowElevation = 2.dp,
