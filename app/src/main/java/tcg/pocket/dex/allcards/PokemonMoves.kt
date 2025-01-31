2bb1f04cb1519621081c60475bf20b80a6a50fbc
sh1mj1 <7wlgns@gmail.com>
2025-01-31 22:21:06 +0900

    refactor(PokemonMoves): reuse PocketDexSectionHeader

--- app/src/main/java/tcg/pocket/dex/allcards/PokemonMoves.kt
+++ app/src/main/java/tcg/pocket/dex/allcards/PokemonMoves.kt
@@ -7,18 +7,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import tcg.pocket.dex.component.PocketDexSectionHeader
 import tcg.pocket.dex.tierdecks.fakeCardDetail
 import tcg.pocket.dex.tierdecks.temporalPokemonTypePlaceholderDrawable
 import tcg.pocket.dex.ui.theme.ChipSize
@@ -30,21 +29,9 @@ fun PokemonMoves(
     modifier: Modifier = Modifier,
 ) {
     Column(modifier = modifier) {
-        Surface(
-            tonalElevation = 2.dp,
-            shadowElevation = 2.dp,
-            modifier = Modifier.fillMaxWidth(),
-            shape = RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp),
-        ) {
-            Text(
-                text = "Moves",
-                style =
-                    MaterialTheme.typography.titleMedium.copy(
-                        fontWeight = FontWeight.Bold,
-                    ),
-                modifier = Modifier.padding(4.dp),
-            )
-        }
+        PocketDexSectionHeader(
+            text = "Moves",
+        )
 
         Surface(
             tonalElevation = 2.dp,
@@ -63,9 +50,9 @@ fun PokemonMoves(
                                     contentDescription = energy.type,
                                     modifier = Modifier.size(ChipSize.Small),
                                     placeholder =
-                                        painterResource(
-                                            temporalPokemonTypePlaceholderDrawable,
-                                        ),
+                                    painterResource(
+                                        temporalPokemonTypePlaceholderDrawable,
+                                    ),
                                 )
                             }
                             Spacer(modifier = Modifier.width(8.dp))
