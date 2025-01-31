e266f244e1b0450323ed68a0d6f4f97d64152ddd
sh1mj1 <7wlgns@gmail.com>
2025-01-31 19:31:12 +0900

    feat(CardDetailHeader): separate CardDetailHeader composable

---
+++ app/src/main/java/tcg/pocket/dex/allcards/CardDetailHeader.kt
@@ -0,0 +1,63 @@
+package tcg.pocket.dex.allcards
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import tcg.pocket.dex.tierdecks.fakeCardDetail
+import tcg.pocket.dex.tierdecks.temporalPokemonCardPlaceholderDrawable
+import tcg.pocket.dex.ui.theme.TcgPocketDexTheme
+
+@Composable
+fun CardDetailHeader(
+    cardDetail: CardDetail,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            AsyncImage(
+                model = cardDetail.imageUrl,
+                contentDescription = null,
+                modifier =
+                    Modifier
+                        .weight(0.8f),
+                placeholder = painterResource(temporalPokemonCardPlaceholderDrawable),
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+
+            CardAttribute(
+                cardDetail = cardDetail,
+                modifier = Modifier.weight(1.2f),
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        PokemonMoves(
+            pokemonMoves = cardDetail.pokemonMoves,
+            modifier = Modifier.fillMaxWidth(),
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CardDetailHeaderPreview() {
+    TcgPocketDexTheme {
+        CardDetailHeader(
+            cardDetail = fakeCardDetail,
+        )
+    }
+}
