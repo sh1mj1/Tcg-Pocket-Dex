2d97f98bd12560159e31011086290bea21951d95
sh1mj1 <7wlgns@gmail.com>
2025-04-08 18:13:23 +0900

    feat(EnglishStringMatcher): refactor to use Rabin-Karp algorithm for substring matching and add RabinKarpMatcher class with tests

--- app/src/main/java/tcg/pocket/dex/EnglishStringMatcher.kt
+++ app/src/main/java/tcg/pocket/dex/EnglishStringMatcher.kt
@@ -1,7 +1,9 @@
 package tcg.pocket.dex
 
-class EnglishStringMatcher {
-    fun isMatched(
+class EnglishStringMatcher: StringMatcher {
+    private val rabinKarpMatcher by lazy { RabinKarpMatcher() }
+
+    override fun isMatched(
         target: String,
         search: String,
     ): Boolean {
@@ -13,47 +15,21 @@ class EnglishStringMatcher {
         val targetWords = normalizedTarget.split(" ")
 
         if (normalizedSearch.length == 1) {
-            return targetWords.first().startsWith(normalizedSearch)
+            return targetWords.first()[0] == normalizedSearch[0]
         }
 
         if (normalizedSearch.length >= 4) {
-            if (rabinKarp(normalizedTarget, normalizedSearch)) return true
+            if (rabinKarpMatcher.contains(
+                    text = normalizedTarget,
+                    pattern = normalizedSearch,
+                )
+            ) {
+                return true
+            }
         }
 
         return targetWords.any { word ->
             word.startsWith(normalizedSearch)
         }
-    }
-
-    private fun rabinKarp(
-        text: String,
-        pattern: String,
-    ): Boolean {
-        val base = 256
-        val prime = 101
-        val m = pattern.length
-        val n = text.length
-
-        var patternHash = 0
-        var textHash = 0
-        var hash = 1
-
-        repeat(m) { hash = (hash * base) % prime }
-
-        (0 until m).forEach { i ->
-            patternHash = (base * patternHash + pattern[i].code) % prime
-            textHash = (base * textHash + text[i].code) % prime
-        }
-
-        (0..(n - m)).forEach { i ->
-            if (patternHash == textHash && text.substring(i, i + m) == pattern) {
-                return true
-            }
-            if (i < n - m) {
-                textHash = (base * (textHash - text[i].code * hash) + text[i + m].code) % prime
-                if (textHash < 0) textHash += prime
-            }
-        }
-        return false
     }
 }
