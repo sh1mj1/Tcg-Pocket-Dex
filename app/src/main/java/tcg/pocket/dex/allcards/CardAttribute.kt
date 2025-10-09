8f634040a8f477e3cd33bb282571e5169aa66b38
sh1mj1 <7wlgns@gmail.com>
2025-10-10 06:17:39 +0900

    feat(CardDetail): Improve layout and handle different card types
    - feat(CardAttribute): Display effect for non-Pokemon cards and hide empty attributes.
    - feat(CardDetail): Adjust `CardAttribute` height to match the card image height.
    - feat(PokemonMovesSection): Hide the "Moves" section if there are no moves.
    - style(CardDetailScreen): Wrap screen content in a `Surface` to apply the theme's background color.
    - test(CardAttribute): Add preview for a Trainer card.

--- app/src/main/java/tcg/pocket/dex/allcards/CardAttribute.kt
+++ app/src/main/java/tcg/pocket/dex/allcards/CardAttribute.kt
@@ -34,12 +34,25 @@ fun CardAttribute(
         shadowElevation = 2.dp,
     ) {
         Column {
-            PokemonCardRarity(cardDetail)
-            PokemonType(cardDetail)
-            PokemonWeakness(cardDetail)
-            PokemonHp(cardDetail)
-            PokemonRetreatCost(cardDetail)
-            PokemonEvolutionStage(cardDetail)
+            with(cardDetail) {
+                if (rarity != "None") {
+                    PokemonCardRarity(cardDetail)
+                }
+                if (category == "Pokemon") {
+                    PokemonType(cardDetail)
+                    PokemonWeakness(cardDetail)
+                    PokemonHp(cardDetail)
+                    PokemonRetreatCost(cardDetail)
+                    PokemonEvolutionStage(cardDetail)
+                } else {
+                    effect?.let {
+                        Text(
+                            text = it,
+                            modifier = Modifier.padding(8.dp),
+                        )
+                    }
+                }
+            }
         }
     }
 }
@@ -196,12 +209,22 @@ private fun PokemonEvolutionStage(cardDetail: CardDetail) {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "Pokemon Card")
 @Composable
-private fun CardAttributePreview() {
+private fun PokemonCardAttributePreview() {
     TcgPocketDexTheme {
         CardAttribute(
             cardDetail = FakeCardsRepo.fakeCardDetail,
+        )
+    }
+}
+
+@Preview(showBackground = true, name = "Trainer Card")
+@Composable
+private fun TrainerCardAttributePreview() {
+    TcgPocketDexTheme {
+        CardAttribute(
+            cardDetail = FakeCardsRepo.fakeTrainerCardDetail,
         )
     }
 }
--- app/src/main/java/tcg/pocket/dex/allcards/CardDetailMetaDataSection.kt
+++ app/src/main/java/tcg/pocket/dex/allcards/CardDetailMetaDataSection.kt
@@ -3,11 +3,19 @@ package tcg.pocket.dex.allcards
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,6 +29,10 @@ fun CardDetailMetaDataSection(
     cardDetail: CardDetail,
     modifier: Modifier = Modifier,
 ) {
+    // TODO: Find a way to avoid calculating density or height using internal variables
+    var imageHeightPx by rememberSaveable { mutableStateOf(0) }
+    val density = LocalDensity.current
+
     Column(modifier = modifier) {
         Row(
             modifier = Modifier.fillMaxWidth(),
@@ -31,14 +43,22 @@ fun CardDetailMetaDataSection(
                 contentDescription = null,
                 modifier =
                     Modifier
-                        .weight(0.8f),
+                        .weight(0.8f)
+                        .onSizeChanged { size ->
+                            imageHeightPx = size.height
+                        },
                 placeholder = painterResource(temporalPokemonCardPlaceholderDrawable),
             )
             Spacer(modifier = Modifier.width(16.dp))
 
+            val imageHeightDp = with(density) { imageHeightPx.toDp() }
+
             CardAttribute(
                 cardDetail = cardDetail,
-                modifier = Modifier.weight(1.2f),
+                modifier =
+                    Modifier
+                        .weight(1.2f)
+                        .defaultMinSize(minHeight = imageHeightDp),
             )
         }
     }
